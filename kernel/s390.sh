6ce5b46b62101ee0dad9301eaf15b62c1b80dfcb
Colin Watson <cjwatson@debian.org>
2004-12-07 14:04:02 +0000

    Explicitly disallow -s390-tape and -s390x (documenting why).
    r24289

--- kernel/s390.sh
+++ kernel/s390.sh
@@ -3,7 +3,20 @@ arch_get_kernel_flavour () {
 }
 
 arch_check_usable_kernel () {
-	return 0
+	case "$1" in
+		*-s390-tape)
+			# Bastian Blank says: "-s390-tape is only a kernel
+			# image without any logic and modules".
+			return 1 ;;
+		*-s390x)
+			# Bastian Blank says that 2.4 -s390x isn't
+			# automatically installable, and 2.6 isn't currently
+			# usable from d-i due to different hardware
+			# configuration.
+			return 1 ;;
+		*)
+			return 0 ;;
+	esac
 }
 
 arch_get_kernel () {
