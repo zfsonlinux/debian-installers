eefabe2fed1019ac84fb3963635e219fe68e83af
Frans Pop <fjp@debian.org>
2005-04-22 12:08:15 +0000

    sparc32 kernels are not usable on sparc64
    r26988

--- kernel/sparc.sh
+++ kernel/sparc.sh
@@ -7,16 +7,20 @@ arch_get_kernel_flavour () {
 }
 
 arch_check_usable_kernel () {
-	if expr "$1" : '.*-sparc32.*' >/dev/null; then
+	case "$2" in
+	    sparc32)
 		if expr "$1" : '.*-2\.6.*-sparc32-smp' >/dev/null; then
 			# No working SMP yet
 			return 1
-		else
-			return 0
 		fi
-	fi
-	if [ "$2" = sparc32 ]; then return 1; fi
-	if expr "$1" : '.*-sparc64.*' >/dev/null; then return 0; fi
+		if expr "$1" : '.*-sparc32.*' >/dev/null; then return 0; fi
+		return 1
+		;;
+	    sparc64)
+		if expr "$1" : '.*-sparc64.*' >/dev/null; then return 0; fi
+		return 1
+		;;
+	esac
 
 	# default to usable in case of strangeness
 	warning "Unknown kernel usability: $1 / $2"
