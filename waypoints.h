59531ef935bf57d4999854b0e2da0f2664fc98ac
Colin Watson <cjwatson@debian.org>
2005-05-23 10:43:26 +0000

    whitespace
    r27896

--- waypoints.h
+++ waypoints.h
@@ -10,10 +10,10 @@ static struct waypoint waypoints[] = {
 	{ 0,	0,	"START" },	/* dummy entry, required */
 	{ 0,	1,	"DOWNREL" },	/* downloading release files; very quick */
 	{ 1,	5,	"DOWNPKGS" },	/* downloading packages files; time varies
-	                           by bandwidth (and size); low granularity */
+		                           by bandwidth (and size); low granularity */
 	{ 5,	10,	"SIZEDEBS" },   /* getting packages sizes; high granularity */
 	{ 10,	25,	"DOWNDEBS" },   /* downloading packages; run time varies by
-				   bandwidth; high granularity */
+					   bandwidth; high granularity */
 	{ 25,	45,	"EXTRACTPKGS" },/* extracting the core packages */
 
 	/* old debootstrap with poor granularity */
