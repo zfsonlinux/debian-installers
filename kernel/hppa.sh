ad3f6facf791efdee6646c28fa0db029a82289ac
Bdale Garbee <bdale@debian.org>
2005-09-21 19:05:35 +0000

    2.6.12 support for hppa, and logic for SMP selection on hppa
    r30709

--- kernel/hppa.sh
+++ kernel/hppa.sh
@@ -7,9 +7,9 @@ arch_get_kernel_flavour () {
 }
 
 arch_check_usable_kernel () {
-	if expr "$1" : '.*-32.*' >/dev/null; then return 0; fi
+	if expr "$1" : '.*-parisc.*' >/dev/null; then return 0; fi
 	if [ "$2" = parisc32 ]; then return 1; fi
-	if expr "$1" : '.*-64.*' >/dev/null; then return 0; fi
+	if expr "$1" : '.*-parisc64.*' >/dev/null; then return 0; fi
 
 	# default to usable in case of strangeness
 	warning "Unknown kernel usability: $1 / $2"
@@ -22,10 +22,16 @@ arch_get_kernel () {
 		2.6)	version="$KERNEL_ABI" ;;
 		*)	warning "Unknown kernel major '$KERNEL_MAJOR'." ;;
 	esac
-	# Don't know how to detect whether SMP is needed, but
-	# apparently it's OK to assume SMP.
+
+	CPUS="$(grep ^processor /proc/cpuinfo | tail -n 1 | cut -d: -f2)"
+	if [ -z "$CPUS" ] || [ "$CPUS" -ne 0 ]; then
+		SMP=-smp
+	else
+		SMP=
+	fi
+
 	case "$1" in
-		parisc32)	echo "kernel-image-$version-32-smp" ;;
-		parisc64)	echo "kernel-image-$version-64-smp" ;;
+		parisc32)	echo "linux-image-$version-parisc$SMP" ;;
+		parisc64)	echo "linux-image-$version-parisc64$SMP" ;;
 	esac
 }
