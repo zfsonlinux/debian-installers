1df2e3479ff9e87d4976de03a5de89b7b89884e7
Bastian Blank <waldi@debian.org>
2006-05-30 19:47:46 +0000

    * kernel/s390.sh: Allow s390x to be selected. * kernel/tests/s390/raptor.test: Update.
    r37683

--- kernel/s390.sh
+++ kernel/s390.sh
@@ -9,18 +9,12 @@ arch_check_usable_kernel () {
 			# Bastian Blank says: "-s390-tape is only a kernel
 			# image without any logic and modules".
 			return 1 ;;
-		*-s390x)
-			# Bastian Blank says that 2.4 -s390x isn't
-			# automatically installable, and 2.6 isn't currently
-			# usable from d-i due to different hardware
-			# configuration.
-			return 1 ;;
 		*)
 			return 0 ;;
 	esac
 }
 
 arch_get_kernel () {
-	echo "kernel-image-$KERNEL_ABI-s390"
+	echo "linux-image-$KERNEL_ABI-$1"
 }
 
