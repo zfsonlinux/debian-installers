76351a1ab49e8476ed93b833c6395a23a91cd130
Frans Pop <fjp@debian.org>
2007-07-25 23:53:19 +0000

    * Remove support for sparc32.
    r48715

--- kernel/sparc.sh
+++ kernel/sparc.sh
@@ -1,30 +1,11 @@
 arch_get_kernel_flavour () {
-	case "$MACHINE" in
-		sparc)		echo sparc32 ;;
-		sparc64)	echo sparc64 ;;
-	esac
+	echo "$MACHINE"
 	return 0
 }
 
 arch_check_usable_kernel () {
-	case "$2" in
-	    sparc32)
-		if expr "$1" : '.*-2\.6.*-sparc32-smp' >/dev/null; then
-			# No working SMP yet
-			return 1
-		fi
-		if expr "$1" : '.*-sparc32.*' >/dev/null; then return 0; fi
-		return 1
-		;;
-	    sparc64)
-		if expr "$1" : '.*-sparc64.*' >/dev/null; then return 0; fi
-		return 1
-		;;
-	esac
-
-	# default to usable in case of strangeness
-	warning "Unknown kernel usability: $1 / $2"
-	return 0
+	if expr "$1" : '.*-sparc64.*' >/dev/null; then return 0; fi
+	return 1
 }
 
 arch_get_kernel () {
@@ -33,12 +14,7 @@ arch_get_kernel () {
 	CPUS=`grep 'ncpus probed' "$CPUINFO" | cut -d: -f2`
 	TYPE=`grep '^type' "$CPUINFO" | head -n1 | cut -d: -f2 | sed -e 's/^[[:space:]]//'`
 	if [ "$CPUS" -ne 1 ] || [ "$TYPE" = "sun4v" ]; then
-		if [ "$1" = sparc32 ] && [ "$KERNEL_MAJOR" = 2.6 ]; then
-			# No working SMP yet
-			:
-		else
-			echo "$imgbase-$KERNEL_MAJOR-$1-smp"
-		fi
+		echo "$imgbase-$KERNEL_MAJOR-$1-smp"
 	fi
 	echo "$imgbase-$KERNEL_MAJOR-$1"
 }
