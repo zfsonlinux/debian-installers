f8856f3701f6a8e103fa0b18f9961e89b4a4b9d3
Martin Michlmayr <tbm@debian.org>
2009-03-22 19:46:51 +0000

    Add support for Marvell's Kirkwood platform.
    r57943

--- kernel/armel.sh
+++ kernel/armel.sh
@@ -1,6 +1,6 @@
 arch_get_kernel_flavour () {
 	case "$SUBARCH" in
-	    iop32x|iop33x|ixp4xx|orion5x|versatile)
+	    iop32x|iop33x|ixp4xx|kirkwood|orion5x|versatile)
 		echo "$SUBARCH"
 		return 0 ;;
 	    ads)
