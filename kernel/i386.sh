fc59ae3f2d24e62ac4d83fec0f14f84da8fee7fe
Ben Hutchings <ben@decadent.org.uk>
2012-05-12 21:54:35 +0100

    base-installer: Disable selection of '686' kernel flavour for i386
    In wheezy, linux-image-686 is now a transitional package depending on
    linux-image-686-pae, and is uninstallable on systems without PAE.
    We must not select it on systems without PAE.
    
    This will prevent installing the squeeze 686 flavour from the wheezy
    installer, but this shouldn't matter since either the 486 or
    686-bigmem flavour is likely to be preferable.

--- kernel/i386.sh
+++ kernel/i386.sh
@@ -15,63 +15,22 @@ arch_get_kernel_flavour () {
 		HAVE_PAE=n
 	fi
 
-	# Should we prefer a PAE/amd64 kernel - is there RAM above 4GB?
-	local WANT_PAE
-	if [ -z "$RAM_END" ]; then
-		local MAP MAP_END
-		RAM_END=0
-		for MAP in /sys/firmware/memmap/* ; do
-			if [ "$(cat $MAP/type)" = "System RAM" ]; then
-				MAP_END="$(cat $MAP/end)"
-				if [ $(($MAP_END > $RAM_END)) = 1 ]; then
-					RAM_END=$MAP_END
-				fi
-			fi
-		done
-	fi
-	if [ $(($RAM_END > 0x100000000)) = 1 ]; then
-		WANT_PAE=y
-	else
-		WANT_PAE=n
-	fi
-	# or is the installer running a PAE kernel?
-	case "$KERNEL_FLAVOUR" in
-	    686-bigmem* | 686-pae*)
-		WANT_PAE=y
-		;;
-	esac
-
-	case "$HAVE_LM$HAVE_PAE$WANT_PAE" in
-	    yyy)
-		echo 686-pae 686-bigmem amd64 686 486
+	case "$HAVE_LM$HAVE_PAE" in
+	    yy)
+		echo 686-pae 686-bigmem amd64 486
 		return 0
 		;;
-	    yyn)
-		echo 686 686-pae 686-bigmem amd64 486
-		return 0
-		;;
-	    yn?)
+	    yn)
 		warning "Processor with LM but no PAE???"
 		;;
-	    nyy)
-		echo 686-pae 686-bigmem 686 486
+	    ny)
+		echo 686-pae 686-bigmem 486
 		return 0
 		;;
-	    nyn)
-		echo 686 686-pae 686-bigmem 486
-		return 0
-		;;
-	    nn?)
-		# Need to check whether 686 is suitable
+	    nn)
+		echo 486
 		;;
 	esac
-
-	# Should we offer a 686 kernel?
-	if grep -q '^flags.*\bfpu\b.*\btsc\b.*\bcx8\b.*\bcmov\b' "$CPUINFO"; then
-		echo 686 486
-	else
-		echo 486
-	fi
 }
 
 arch_check_usable_kernel () {
@@ -80,15 +39,8 @@ arch_check_usable_kernel () {
 	set -- $2
 	while [ $# -ge 1 ]; do
 		case "$NAME" in
-		    *-"$1")
-			return 0;
-			;;
-		    *-"$1"-bigmem* | *-"$1"-pae*)
-			# Don't allow -bigmem or -pae suffix, as these
-			# require extra CPU features
-			;;
-		    *-"$1"-*)
-			# Do allow any other hyphenated suffix
+		    *-"$1" | *-"$1"-*)
+			# Allow any other hyphenated suffix
 			return 0
 			;;
 		esac
