aa06a315642727f39af9adb89c2d9e4d15c7e745
Aurelien Jarno <aurelien@aurel32.net>
2014-07-12 16:50:57 +0200

    Group Loongson 3A and 3B in the same loongson-3 subarchitecture.

--- kernel/mipsel.sh
+++ kernel/mipsel.sh
@@ -1,6 +1,6 @@
 arch_get_kernel_flavour () {
 	case "$SUBARCH" in
-	    4kc-malta|5kc-malta|loongson-2e|loongson-2f|loongson-3a|sb1-bcm91250a|sb1a-bcm91480b)
+	    4kc-malta|5kc-malta|loongson-2e|loongson-2f|loongson-3|sb1-bcm91250a|sb1a-bcm91480b)
 		echo "$SUBARCH"
 		return 0 ;;
 	    *)
